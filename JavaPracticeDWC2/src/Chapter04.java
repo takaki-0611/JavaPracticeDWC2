3981156031b73f44f2e08cdb5c163a36ae82d76e
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-03-06 23:23:54 +0900

    3/6practice

---
+++ JavaPracticeDWC2/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 8;
+		String str1 = "YU";
+		System.out.println(str1);
+		str1 = "laplus";
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
