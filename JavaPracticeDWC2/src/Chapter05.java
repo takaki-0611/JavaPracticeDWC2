be885d9224e198db3b047622af427e6a36ffa531
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-03-09 23:43:44 +0900

    3/9practice

---
+++ JavaPracticeDWC2/src/Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+  public static void main(String[] args) {
+    long l = 100000000L;
+    System.out.println(l);
+    double d = 222;
+    System.out.println(d);
+    boolean b = true;
+    System.out.println(b);
+
+    Integer i = 151;
+    System.out.println(i);
+    Float f = 2.222222f;
+    System.out.println(f);
+    Character c = 'し';
+    System.out.println(c);
+
+    int[] array = { 1, 2, 4, 7, 11, 16 };
+    System.out.println(array[3]);
+  }
+}
