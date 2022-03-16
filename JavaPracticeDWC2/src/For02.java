27b10e38bfecd2cd661935eb4f3c9848ded23382
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-03-16 20:05:07 +0900

    3/16practice

---
+++ JavaPracticeDWC2/src/For02.java
@@ -0,0 +1,8 @@
+public class For02 {
+  public static void main(String[] args) {
+    int[] array = { 1, 2, 4, 8, 16 };
+    for (int number : array) {
+      System.out.println("For02 =" + number);
+    }
+  }
+}
