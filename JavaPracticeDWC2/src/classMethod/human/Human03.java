b9cd9d9034448c76d95ce3696b8416c7747852e7
takaki-0611 <pq.earlyripe.earlyrotten@gmail.com>
2022-03-21 23:34:56 +0900

    3/21practice

--- JavaPracticeDWC2/src/classMethod/human/Human03.java
+++ JavaPracticeDWC2/src/classMethod/human/Human03.java
@@ -9,17 +9,17 @@ public class Human03 {
     this.name = "山田";
     this.age = 20;
     this.profession = "プログラマー";
+  }
 
-    public String getProfile() {
-      return "年齢は" + this.age + "、職業は" + this.profession;
-    }
+  public String getProfile() {
+    return "年齢は" + this.age + "、職業は" + this.profession;
+  }
 
-    public void greet(String friend) {
-      if (friend == null) {
-        System.out.println("挨拶する友達がわかりません。");
-        return;
-      }
-      System.out.println(friend + "さん、こんにちは！");
+  public void greet(String friend) {
+    if (friend == null) {
+      System.out.println("挨拶する友達がわかりません！");
+      return;
     }
+    System.out.println(friend + "さん、こんにちは！");
   }
 }
